91e389813dfb5b804a3f58de8b4bd0e489c9546b
Brian Caudill <brian.m.caudill@gmail.com>
2023-09-20 09:08:01 -0300

    Update code.js
    added the retailer name to insertId and source

--- code.js
+++ code.js
@@ -1,12 +1,25 @@
+function getRetailer(){
+    // Find the retailer name
+let element = document.querySelector('.inline-flex.-mt-1.text-4xl.font-extrabold');
+retailer = "";
+// Check if the element exists
+if (element) {
+  // Get the text content of the element
+  retailer = element.textContent.trim();
+  } else {
+  retailer = "";
+    }
+    return retailer
+}
 // Create a dynamic lightbox modal
-const createModal = () => {
-    const modal = document.createElement('div');
+function createModal() {
+    let modal = document.createElement('div');
     modal.className = 'modal';
     document.body.appendChild(modal);
-    const modalContent = document.createElement('div');
+    let modalContent = document.createElement('div');
     modalContent.className = 'modal-content';
     modal.appendChild(modalContent);
-    const messageElement = document.createElement('p');
+    let messageElement = document.createElement('p');
     messageElement.textContent = 'File created and downloaded successfully! Check your downloads for offers.json file!';
     messageElement.style.fontSize = '18px';
     messageElement.style.color = 'green';
@@ -32,13 +45,13 @@ const createModal = () => {
 };
 // Parse the current page and create the JSON file
 async function parseCurrentPageAndCreateFile() {
-    const modal = createModal();
-    const allParsedOffers = await clickNextButtonAndParse();
-    const jsonString = JSON.stringify(allParsedOffers, null, 2);
-    const blob = new Blob([jsonString], {
+    let modal = createModal();
+    let allParsedOffers = await clickNextButtonAndParse();
+    let jsonString = JSON.stringify(allParsedOffers, null, 2);
+    let blob = new Blob([jsonString], {
         type: 'application/json'
     });
-    const downloadLink = document.createElement('a');
+    let downloadLink = document.createElement('a');
     downloadLink.href = URL.createObjectURL(blob);
     downloadLink.download = 'offers.json';
     downloadLink.click();
@@ -50,40 +63,57 @@ async function parseCurrentPageAndCreateFile() {
     // Change the timeout duration as needed
 }
 async function clickNextButtonAndParse() {
-    const nextButton = document.querySelector('a[aria-label="Next Page"]');
+    let nextButton = document.querySelector('a[aria-label="Next Page"]');
     if (nextButton && nextButton.getAttribute('aria-label') !== 'Next Page, already on last page') {
-        const currentPageOffers = await parseCurrentPage();
+        let currentPageOffers = await parseCurrentPage();
         nextButton.click();
         await new Promise((resolve) => setTimeout(resolve, 100));
-        const nextPageOffers = await clickNextButtonAndParse();
-        const allOffers = currentPageOffers.concat(nextPageOffers);
+        let nextPageOffers = await clickNextButtonAndParse();
+        let allOffers = currentPageOffers.concat(nextPageOffers);
         return allOffers;
     } else {
         return parseCurrentPage();
     }
 }
 async function parseCurrentPage() {
-    const cards = document.querySelectorAll('.item-offer-card');
-    const offers = [];
+    let cards = document.querySelectorAll('.item-offer-card');
+    let offers = [];
+    let retailer = getRetailer().toUpperCase();
+  
     cards.forEach((card) => {
-        const cashBackElement = card.querySelector('.item-offer-card__text--cash-back');
-        const offerNameElement = card.querySelector('.item-offer-card__text--offer-name');
-        const offerDetailsElement = card.querySelector('.item-offer-card__text--subtext');
-        const cashBack = cashBackElement ? cashBackElement.textContent.trim() : '';
-        const offerName = offerNameElement ? offerNameElement.textContent.trim() : '';
-        const offerDetails = offerDetailsElement ? offerDetailsElement.textContent.trim() : '';
-        offers.push({
-            cashBack,
-            offerName,
-            offerDetails,
-            expiration: "NO EXPIRATION",
-            insertDate: "DIGITAL",
-            insertId: "IBOTTA",
-            url: "https://ibotta.com",
-            categories: "Food",
-            source: "IBOTTA",
-            couponId: Math.random().toString(36).substring(7)
-        });
+        let cashBackElement = card.querySelector('.item-offer-card__text--cash-back');
+        let offerNameElement = card.querySelector('.item-offer-card__text--offer-name');
+        let offerDetailsElement = card.querySelector('.item-offer-card__text--subtext');
+        let cashBack = cashBackElement ? cashBackElement.textContent.trim() : '';
+        let offerName = offerNameElement ? offerNameElement.textContent.trim() : '';
+        let offerDetails = offerDetailsElement ? offerDetailsElement.textContent.trim() : '';
+        if (retailer !== "") {
+  offers.push({
+    cashBack,
+    offerName,
+    offerDetails,
+    expiration: "NO EXPIRATION",
+    insertDate: "DIGITAL",
+    insertId: `IBOTTA-${retailer}`,
+    url: "https://ibotta.com",
+    categories: "Food",
+    source: `IBOTTA-${retailer}`,
+    couponId: Math.random().toString(36).substring(7)
+  });
+} else {
+  offers.push({
+    cashBack,
+    offerName,
+    offerDetails,
+    expiration: "NO EXPIRATION",
+    insertDate: "DIGITAL",
+    insertId: "IBOTTA",
+    url: "https://ibotta.com",
+    categories: "Food",
+    source: "IBOTTA",
+    couponId: Math.random().toString(36).substring(7)
+  });
+}
     });
     return offers;
 } // Call the parseCurrentPageAndCreateFile function to start parsing and file creation
